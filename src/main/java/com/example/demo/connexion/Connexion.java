5f5102b672bcc35d09731738d4dceef75363803a
Sebastien Patau <sebastienpatau@gmail.com>
2017-11-29 10:55:34 +0100

    Commit initial

---
+++ src/main/java/com/example/demo/connexion/Connexion.java
@@ -0,0 +1,24 @@
+package com.example.demo.connexion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Connexion {
+
+	
+	 @GetMapping("/")
+	    public String index() {
+	        return "index";
+	    }
+	 
+	  @GetMapping("/connexion")
+	    public String connexion() {
+	        return "connexion";
+	    }
+	  
+	  @GetMapping("/readmember")
+	    public String readmember() {
+	        return "readmember";
+	    }
+}
