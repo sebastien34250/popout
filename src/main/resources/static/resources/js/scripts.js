63d236facd5c1efb9a48dd38172c992a964df13a
Romain Gelly <gellyrom@gmail.com>
2017-12-07 15:36:46 +0100

    bidouilles

--- src/main/resources/static/resources/js/scripts.js
+++ src/main/resources/static/resources/js/scripts.js
@@ -1,1 +1,13 @@
-// Empty JS for your own code to be here
+var password = document.getElementById(exampleInputPassword1);
+var passwordConf = document.getElementById(exampleInputPassword2);
+
+function validatePassword() { 
+	if (password.value != passwordConf.value) {
+		passwordConf.setCustomValidity("Pas de correspondance");
+	} else {
+		passwordConf.setCustomValidity('');
+	}
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
